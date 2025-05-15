cbcc2404bb89435f8db4ff017abdfbe45cb6116f
hungvannguyen <nguyenvanhung08012002@gmail.com>
2025-05-15 16:18:38 +0700

    chore: add Prettier configuration and update Tailwind CSS dependencies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,12 @@
 <!doctype html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-    @vite('resources/css/app.css')
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	@vite('resources/css/app.css')
 </head>
 <body>
-<h1 class="text-3xl font-bold underline">
-Hello world!
-</h1>
+<h1 class="text-3xl lg:text-4xl font-bold underline">
+	Hello world! </h1>
 </body>
 </html>
